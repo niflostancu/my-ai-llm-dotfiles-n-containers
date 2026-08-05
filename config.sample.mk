5134f1ea8b33ade93745f9bfb35e07b72d3f287a
Stancu Florin <niflostancu@gmail.com>
2026-08-05 16:03:28 +0300

    config.local: add sample config, move path to root

---
+++ config.sample.mk
@@ -0,0 +1,10 @@
+# Copy as config.local.mk to customize various Makefile variables
+
+# Set a custom docker image prefix
+#IMAGE_PREFIX = yourdockername/
+
+# llama.cpp target (TODO)
+
+# Pi agent configuration (pi / little / omp)
+#PI_CFG=little
+
